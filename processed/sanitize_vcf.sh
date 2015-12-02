eb3ceecb2b82da60740060f32908ff57a9a23958
Alex Kalicki <alexander.kalicki@gmail.com>
2015-12-02 16:05:37 -0500

    fix sanitize emthod, add sanitized chromosome vcfs

--- processed/sanitize_vcf.sh
+++ processed/sanitize_vcf.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 for vcf_file in chr/vcf/*.vcf; do
+    vcf_sanitized=${vcf_file/.vcf/.sanitized.vcf}
     cat $vcf_file \
         | sed 's/VCFv4.2/VCFv4.1/' \
         | sed 's/,Version=3>/>/' \
         | sed 's/,Version=\"3\">/>/' \
-        | sed 's/Number=R/Number=./' > $vcf_file
-    bgzip $vcf_file
+        | sed 's/Number=R/Number=./' > $vcf_sanitized
+    bgzip $vcf_sanitized
+    mv "$vcf_sanitized.gz" chr/vcf_sanitized
 done
 
 exit 0
